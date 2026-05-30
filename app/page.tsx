ebc9d7a82fd56496fe33a96c4211d64501566e23
killerbsurf-coder <killerbsurf@gmail.com>
2026-05-29 21:52:35 -0400

    Initial commit for Saasy site

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,270 @@
-import Image from "next/image";
+
+"use client"
+import { useEffect, useState } from 'react';
+import '../styles/project-home.css';
+import '../styles/default.css';
+import { Eye, CirclePlus, SquarePen, CircleX, ArrowDownToLine } from 'lucide-react';
+import ResponsiveAppBar from './appbar';
+
+import Footer from '../footer'
+
+
+const download = () => {
+  return (
+    <ArrowDownToLine />
+  );
+};
+
+const create = () => {
+  return (
+    <CirclePlus />
+  );
+};
+const view = () => {
+  return (
+    <Eye />
+  );
+};
+const trash = () => {
+  return (
+    <CircleX />
+  );
+};
+
+const edit = () => {
+  return (
+    <SquarePen />
+  );
+};
 
 export default function Home() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    const lucide = (globalThis as any).lucide;
+    if (lucide?.createIcons) {
+      lucide.createIcons();
+    }
+  }, []);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <>
+      <ResponsiveAppBar />
+
+
+
+      <article>
+
+        <div className="max-w-7xl mx-auto">
+          <h1 className="max-w-m text-4xl font-semibold" style={{ marginTop: '20px' }}>SaaSy Marketing Team</h1>
+          <h5 className="max-w-m text-2xl font-condensed-light leading-relaxed" style={{ marginBottom: '20px' }}>Marketing that works for You!</h5>
+          <div className="flex flex-col md:flex-row md:items-center justify-between mb-8 gap-" style={{
+            backgroundColor: '#ffffff',
+            padding: 20,
+            borderRadius: 10,
+            border: 'thin solid #e5e7eb',
+            marginBottom: 20,
+          }}>
+
+            <div>
+              <h3 className="text-2xl font-bold text-slate-900">Sales and Profit</h3>
+              <p className="text-slate-500 text-sm">Manage, edit, and download your generated data.</p>
+            </div>
+
+
+            <button className="bg-green-800 hover:bg-green-600 text-white px-4 py-2 rounded-sm text-sm font-medium transition-colors flex items-center gap-2" title="Create New">  <CirclePlus /> Create New </button>
+          </div>
+
+
+
+          <div className="bg-white rounded-xl shadow-sm border border-slate-200 overflow-hidden">
+            <div className="overflow-x-auto">
+              <table className="w-full text-left border-separate border-spacing-0">
+                <thead>
+                  <tr className="bg-gray-300 text-gray-900 uppercase text-s font-semibold">
+                    <th style={{ width: 163 }} className="px-6 px-6 py-4 border-b border-gray-200">Title</th>
+                    <th style={{ width: 361 }} className="px-6 px-6 py-4 border-b border-gray-200">Description</th>
+                    <th style={{ width: 86 }} className="px-6 px-6 py-4 border-b border-gray-200">Date</th>
+                    <th style={{ width: 66 }} className="px-6 px-6 py-4 border-b border-gray-200 text-right">Actions</th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-slate-100">
+
+                  <tr className="hover:bg-gray-100 transition-colors">
+                    <td className="px-6 py-4 font-semibold text-slate-800">Change History Report</td>
+                    <td className="px-6 py-4 text-sm text-slate-500 max-w-xs truncate">Tracks changes to the OS environment.</td>
+                    <td className="px-6 py-4 text-sm text-slate-600 whitespace-nowrap">Feb 02, 2024</td>
+                    <td className="px-6 px-6 py-4 text-right"><div className="flex justify-end gap-2">
+                      <button title="View" className="p-2 text-gray-600 hover:bg-slate-200 rounded-sm transition-colors">  <Eye /></button>
+                      <button title="Edit" className="p-2 text-gray-600 hover:bg-amber-50 rounded-sm transition-colors"> <SquarePen /></button>
+                      <button title="Delete" className="p-2 text-gray-600 hover:bg-red-50 rounded-sm transition-colors"> <CircleX /></button>
+                      <button title="Download" className="p-2 text-gray-600 hover:bg-red-50 rounded-sm transition-colors"><ArrowDownToLine /></button>
+                    </div></td>
+                  </tr>
+                  <tr className="hover:bg-gray-100 transition-colors">
+                    <td className="px-6 py-4 font-semibold text-slate-800">CVE Compliance</td>
+                    <td className="px-6 px-6 py-4 text-slate-900 text-sm max-w-xs truncate">Identifies systems with known security vulnerabilities....</td>
+                    <td className="px-6 px-6 py-4 text-gray-600 text-sm">Oct 12, 2023</td>
+                    <td className="px-6 px-6 py-4 text-right"><div className="flex justify-end gap-2">
+                      <button title="View" onClick={openModal} className="p-2 text-gray-600 hover:bg-slate-200 rounded-sm transition-colors">  <Eye /></button>
+                      <button title="Edit" className="p-2 text-gray-600 hover:bg-amber-50 rounded-sm transition-colors"> <SquarePen /></button>
+                      <button title="Delete" className="p-2 text-gray-600 hover:bg-red-50 rounded-sm transition-colors"> <CircleX /></button>
+                      <button title="Download" className="p-2 text-gray-600 hover:bg-red-50 rounded-sm transition-colors"><ArrowDownToLine /></button>
+                    </div></td>
+                  </tr>
+                  <tr className="hover:bg-slate-100 transition-colors">
+                    <td className="px-6 py-4 font-semibold text-slate-800">System Health Log</td>
+                    <td className="px-6 px-6 py-4 text-slate-900 text-sm max-w-xs truncate">Server uptime statistics and database latency records for March.</td>
+                    <td className="px-6 px-6 py-4 text-gray-600 text-sm">Mar 05, 2024</td>
+                    <td className="px-6 px-6 py-4 text-right"><div className="flex justify-end gap-2">
+                      <button title="View" className="p-2 text-gray-600 hover:bg-slate-200 rounded-sm transition-colors">  <Eye /></button>
+                      <button title="Edit" className="p-2 text-gray-600 hover:bg-amber-50 rounded-sm transition-colors"> <SquarePen /></button>
+                      <button title="Delete" className="p-2 text-gray-600 hover:bg-red-50 rounded-sm transition-colors"> <CircleX /></button>
+                      <button title="Download" className="p-2 text-gray-600 hover:bg-red-50 rounded-sm transition-colors"><ArrowDownToLine /></button>
+                    </div></td>
+                  </tr>
+                  <tr className="hover:bg-slate-100 transition-colors">
+                    <td className="px-6 py-4 font-semibold text-slate-800">Machines Performance</td>
+                    <td className="px-6 px-6 py-4 text-slate-900 text-sm max-w-xs truncate">Performance stats of machines breakdown ...</td>
+                    <td className="px-6 px-6 py-4 text-gray-600 text-sm">Oct 12, 2024</td>
+                    <td className="px-6 px-6 py-4 text-right"><div className="flex justify-end gap-2">
+                      <button title="View" className="p-2 text-gray-600 hover:bg-slate-200 rounded-sm transition-colors">  <Eye /></button>
+                      <button title="Edit" className="p-2 text-gray-600 hover:bg-amber-50 rounded-sm transition-colors"> <SquarePen /></button>
+                      <button title="Delete" className="p-2 text-gray-600 hover:bg-red-50 rounded-sm transition-colors"><CircleX /></button>
+                      <button title="Download" className="p-2 text-gray-600 hover:bg-red-50 rounded-sm transition-colors"><ArrowDownToLine /></button>
+                    </div></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <div className="bg-slate-50/50 px-6 py-4 border-t border-slate-200 flex items-center justify-between">
+              <p className="text-xs text-slate-500 font-medium">Page 1 of 2</p>
+              <div className="flex gap-2">
+                <button className="px-3 py-1 text-xs font-semibold text-slate-00 cursor-not-allowed">Previous</button>
+                <button className="px-3 py-1 text-xs font-semibold text-cyan-600 hover:text-green-600">Next</button>
+              </div>
+            </div>
+
+            <div
+              id="myModal"
+              className="modal"
+              role="dialog"
+              style={{ display: modalOpen ? 'block' : 'none' }}
+              onClick={(event) => {
+                if (event.target === event.currentTarget) {
+                  closeModal();
+                }
+              }}
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+              <div className="modal-content">
+                <span className="close" onClick={closeModal}>&times;</span>
+                <div className="w-full max-w-6xl mx-auto p-6">
+
+                  <div className="bg-white rounded-xl border border-slate-200 shadow-sm overflow-hidden">
+                    <div className="flex flex-col md:flex-row md:items-center justify-between mb-8 gap-" style={{
+                      backgroundColor: '#ffffff',
+                      padding: 20,
+                      borderRadius: 10,
+                      border: 'thin solid #e5e7eb',
+                    }}>
+                      <div>
+                        <h2 className="text-2xl font-bold text-cyan-800">CVE Compliance Report</h2>
+                      </div>
+                      <div className="flex gap-3">
+                        <button className="px-4 py-1.5 text-sm font-semibold text-slate-600 bg-white border border-slate-200 rounded-sm hover:bg-slate-100 transition-all">Download PDF</button>
+                        <button className="bg-green-800 hover:bg-green-600 text-white px-4 py-2 rounded-sm text-sm font-medium transition-colors flex items-center gap-2">Export CSV</button>
+                      </div>
+                    </div>
+                    <table className="w-full text-left border-collapse min-w-[1000px]">
+                      <thead>
+                        <tr>
+                          <th className="px-6 px-6 py-4 border-b border-gray-200">CVE ID</th>
+                          <th className="px-6 px-6 py-4 border-b border-gray-200">Severity</th>
+                          <th className="px-6 px-6 py-4 border-b border-gray-200">CVSS Score</th>
+                          <th className="px-6 px-6 py-4 border-b border-gray-200">Asset Name/Host</th>
+                          <th className="px-6 px-6 py-4 border-b border-gray-200">Package/Software</th>
+                          <th className="px-6 px-6 py-4 border-b border-gray-200">Status</th>
+                          <th className="px-6 px-6 py-4 border-b border-gray-200">KEV Status</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td className="px-6 py-4">CVE-2023-38831</td>
+                          <td className="severity-critical">Critical</td>
+                          <td className="px-6 py-4">9.8</td>
+                          <td className="px-6 py-4">FIN-SRV-01</td>
+                          <td className="px-6 py-4">WinRAR</td>
+                          <td className="px-6 py-4">In Progress</td>
+                          <td className="px-6 py-4">True</td>
+                        </tr>
+                        <tr>
+                          <td className="px-6 py-4">CVE-2021-44228</td>
+                          <td className="severity-critical">Critical</td>
+                          <td className="px-6 py-4">10.0</td>
+                          <td className="px-6 py-4">APP-WEB-04</td>
+                          <td className="px-6 py-4">log4j-core</td>
+                          <td className="status-fixed">Fixed</td>
+                          <td className="px-6 py-4">True</td>
+                        </tr>
+                        <tr>
+                          <td className="px-6 py-4">CVE-2023-4863</td>
+                          <td className="severity-high">High</td>
+                          <td className="px-6 py-4">8.8</td>
+                          <td className="px-6 py-4">MKT-LAP-12</td>
+                          <td className="px-6 py-4">Google Chrome</td>
+                          <td className="status-new">New</td>
+                          <td className="px-6 py-4">True</td>
+                        </tr>
+                        <tr>
+                          <td className="px-6 py-4">CVE-2024-21626</td>
+                          <td className="severity-high">High</td>
+                          <td className="px-6 py-4">8.6</td>
+                          <td className="px-6 py-4">DKR-NODE-09</td>
+                          <td className="px-6 py-4">runc (Container)</td>
+                          <td className="px-6 py-4">In Progress</td>
+                          <td className="px-6 py-4">False</td>
+                        </tr>
+                        <tr>
+                          <td className="px-6 py-4">CVE-2023-22515</td>
+                          <td className="severity-high">High</td>
+                          <td className="px-6 py-4">8.0</td>
+                          <td className="px-6 py-4">WIKI-PROD-01</td>
+                          <td className="px-6 py-4">Atlassian Confluence</td>
+                          <td className="status-fixed">Fixed</td>
+                          <td className="px-6 py-4">True</td>
+                        </tr>
+                        <tr>
+                          <td className="px-6 py-4">CVE-2022-40684</td>
+                          <td className="severity-medium">Medium</td>
+                          <td className="px-6 py-4">6.5</td>
+                          <td className="px-6 py-4">FIREWALL-HQ</td>
+                          <td className="px-6 py-4">FortiOS</td>
+                          <td className="px-6 py-4">Risk Accepted</td>
+                          <td className="px-6 py-4">False</td>
+                        </tr>
+                        <tr>
+                          <td className="px-6 py-4">CVE-2023-34048</td>
+                          <td className="severity-medium">Medium</td>
+                          <td className="px-6 py-4">5.3</td>
+                          <td className="px-6 py-4">VM-HOST-02</td>
+                          <td className="px-6 py-4">vCenter Server</td>
+                          <td className="status-new">New</td>
+                          <td className="px-6 py-4">False</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </article>
+      <Footer />
+    </>
   );
-}
+};
