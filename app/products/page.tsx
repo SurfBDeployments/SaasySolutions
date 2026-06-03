b50e522c4cc7e288e28c742aacb29ba177840058
killerbsurf-coder <killerbsurf@gmail.com>
2026-06-03 15:21:28 -0400

    Updated text

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <div className="max-w-7xl mx-auto">
           <div className='maincontent'>
             <h1 className="max-w-m text-4xl font-semibold leading-10 tracking-tight text-black dark:text-cyan-800" style={{ textAlign: "left" }}>Executive Products</h1>
-            <h5 className="max-w-m text-3xl font-condensed-light leading-10 tracking-tight text-black dark:text-slate-800" style={{ textAlign: "left", marginBottom: '20px' }}>Showing data for net profit margins, revenue for last 6 months, EBIT and Debt to Equity Ratios and MRR/Churn Data</h5>
+            <h5 className="max-w-m text-3xl font-condensed-light leading-10 tracking-tight text-black dark:text-slate-800" style={{ textAlign: "left", marginBottom: '20px' }}>Showing data for net profit margins, revenue for last 6 months, EBIT and Debt to Equity Ratios, MRR/Churn Dataand GraphQL API</h5>
 
             <MasterDashboard />
 
